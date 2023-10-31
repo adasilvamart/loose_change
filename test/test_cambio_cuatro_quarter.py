37e3e519dd9d167484eae625af302f08a923d633
adasilvamart <adasilva.clase@gmail.com>
2023-10-31 13:35:05 +0100

    pytest.ini modified and create other tests

--- test/test_cambio_cuatro_quarter.py
+++ test/test_cambio_cuatro_quarter.py
@@ -1,6 +1,6 @@
 import pytest
 from src.lose_change import loose_change
 
-@pytest.mark.cambio_cincuenta
-def test_cambio_cincuenta():
+@pytest.mark.cambio_cien
+def test_cambio_cuatro_quarter():
     assert loose_change(100) == {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 4}
